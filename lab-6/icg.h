3c1cb4f85247d26504fbd2c443a37f81ddb5d4f7
pYr0 <pyrotourface@gmail.com>
2024-04-10 02:11:11 +0530

    lab 6 done

---
+++ lab-6/icg.h
@@ -0,0 +1,12 @@
+#ifndef ICG_H
+#define ICG_H
+
+#include <stdio.h>
+
+extern FILE* icg_f;
+extern int temp_no;
+
+void quad_gen(char *op, char *arg1, char *arg2, char *result);
+char *create_temp();
+
+#endif
